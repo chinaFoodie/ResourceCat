8f0c8e8a2c8fa379ee6c4afb465eb2b0ad76c1b1
FoodieOfChina <179972578@qq.com>
2016-05-30 15:55:14 +0800

    adapter

--- app/src/main/java/com/cn/clound/adapter/HistoryMettingRecyclerAdapter.java
+++ app/src/main/java/com/cn/clound/adapter/HistoryMettingRecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.cn.clound.R;
 import com.cn.clound.bean.metting.HistoryMeetingModel;
@@ -36,6 +37,15 @@ public class HistoryMettingRecyclerAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        ((MyViewHolder) holder).tvMeetingName.setText(listMetting.get(position).getName());
+        ((MyViewHolder) holder).tvMeetingTime.setText(listMetting.get(position).getBeginAt() + "-" + listMetting.get(position).getEndAt().substring(listMetting.get(position).getEndAt().indexOf(" ") + 1));
+        if (listMetting.get(position).getTypeStr().equals("1")) {
+            ((MyViewHolder) holder).tvMeetingType.setText("网络会议");
+            ((MyViewHolder) holder).tvMeetingType.setBackgroundResource(R.color.color_meeting_net);
+        } else {
+            ((MyViewHolder) holder).tvMeetingType.setText("实体会议");
+            ((MyViewHolder) holder).tvMeetingType.setBackgroundResource(R.color.color_meeting_off);
+        }
     }
 
     @Override
@@ -44,9 +54,15 @@ public class HistoryMettingRecyclerAdapter extends RecyclerView.Adapter {
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMeetingType;
+        TextView tvMeetingTime;
+        TextView tvMeetingName;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            tvMeetingName = (TextView) itemView.findViewById(R.id.tv_mine_meeting_name);
+            tvMeetingType = (TextView) itemView.findViewById(R.id.tv_mine_meeting_type);
+            tvMeetingTime = (TextView) itemView.findViewById(R.id.tv_mine_meeting_time);
         }
     }
 }
